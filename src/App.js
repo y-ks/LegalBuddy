e142778ca28eb1381566bfa48a589c3c9af2b8d8
y-ks <66983245+y-ks@users.noreply.github.com>
2022-06-28 17:57:59 +0545

    ProtectedRoute Initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "antd/dist/antd.less";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/SignupUser";
@@ -11,13 +11,21 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/signuplawyer" component={SignupLawyer} />
       </Switch>
     </BrowserRouter>
   );
+}
+
+export function ProtectedRoute(props) {
+  if (localStorage.getItem("name")) {
+    return <Route {...props} />;
+  } else {
+    return <Redirect to="/login" />;
+  }
 }
 
 export default App;
